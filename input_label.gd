5b7e3edffed82b92a71db3215e37324bb7916d2e
Hashir Shahzad <hashir447079@gmail.com>
2025-06-13 13:53:50 +0500

    effects :O

--- input_label.gd
+++ input_label.gd
@@ -18,7 +18,21 @@ func set_next_character(next_character_index : int):
 	if next_character_index != prompt.text.length():
 		red_text = enclose_string_in_bbcode(red, prompt.text.substr(next_character_index + 1, prompt.text.length() - next_character_index + 1))
 	
+	red_text = shake_effect(red_text)
+	green_text = shake_effect(green_text)
+	green_text = pulse_effect(green_text)
+	
 	prompt.parse_bbcode(blue_text + green_text + red_text)
 
 func enclose_string_in_bbcode(color : Color, string : String) -> String:
 	return "[color=#" + color.to_html(false) + "]" + string + "[/color]"
+
+func wavy_effect(string: String) -> String:
+	return "[wave amp=50.0 freq=5.0 connected=1]" + string + "[/wave]"
+
+func shake_effect(string: String) -> String:
+	return "[shake rate=20.0 level=5 connected=1]" + string + "[/shake]"
+
+func pulse_effect(string: String) -> String:
+	return "[pulse freq=3 color=#ffffff40 ease=-2.0]" + string + "[/pulse]"
+	
